ddf2b5c79a7cf273a69f3a8c377c14c4a90f66c8
cortecom <38658157+cortecom@users.noreply.github.com>
2026-09-22 17:09:23 -0300

    Update audio references and scene transitions

--- js/script.js
+++ js/script.js
@@ -872,6 +872,7 @@ monogatari.assets ('sounds', {
     'low_vibration': 'low_vibration.ogg',
     // Minijuego Guardianes
     'eco': 'eco.ogg',
+	'capitulo1': 'eco.ogg',
     'viento': 'viento.ogg',
     'vibracion': 'vibracion.ogg',
 	// Las 7 maravillas
@@ -1540,7 +1541,7 @@ monogatari.script({
         showInventoryBar();
     },
 
-     'jump Escena24'
+     'jump Capitulo1'
     ],
 
 /* =====================================================
@@ -1551,7 +1552,7 @@ monogatari.script({
 	'Capitulo1': [
 		'show scene negro with fadeIn duration 1s',
 		'centered <h1>Capítulo I</h1><p>El Origen Oculto</p>',
-		//'play music capitulo1 with loop fade 2',
+		'play music capitulo1 with loop fade 2',
 		'jump Escena1_QuintaNormal'   // primera escena real del capítulo
 	],
 
